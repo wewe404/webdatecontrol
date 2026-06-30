b2f9697394a71eb46caf3045203951d77b150dd5
AlannaFish <2945799725@qq.com>
2026-06-30 08:48:53 +0800

    Day2: 实现网卡枚举和打开网卡

--- src/main.c
+++ src/main.c
@@ -1,10 +1,33 @@
+#include <stdio.h>
+#include <pcap.h>
 #include "capture.h"
 
 /* 程序入口 */
 int main()
 {
-    /* 测试初始化函数 */
-    capture_init("eth0");
+    char errbuf[PCAP_ERRBUF_SIZE];
+    pcap_if_t *alldevs;
+
+    list_devices();
+
+    if (pcap_findalldevs(&alldevs, errbuf) == -1) {
+        printf("获取网卡失败：%s\n", errbuf);
+        return 1;
+    }
+
+    if (alldevs == NULL) {
+        printf("没有找到可用网卡。\n");
+        return 1;
+    }
+
+    pcap_t *handle = capture_init(alldevs->name);
+
+    if (handle != NULL) {
+        start_capture(handle);
+        stop_capture(handle);
+    }
+
+    pcap_freealldevs(alldevs);
 
     return 0;
 }
